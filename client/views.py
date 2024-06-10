b13eb7f23876d8084bf09c69fe94259c1048813c
saei1d <hehehe3bar@gmail.com>
2024-06-10 13:37:22 +0330

    home

--- client/views.py
+++ client/views.py
@@ -7,24 +7,20 @@ from client.models import CustomUser
 def auth(request):
     context = {}
     if request.method == 'POST':
-        print(1)
         username = request.POST.get('n_code')
         password = request.POST.get('phone')
+        print(username, password)
         if CustomUser.objects.filter(username=username).exists():
             user = authenticate(request, username=username, password=password)
             if user:
-                print(user)
                 login(request, user)
-                print(4)
                 return redirect(reverse('client:home'))
             else:
-                print("شماره همراه / ایمیل  یا کلمه عبور اشتباه است")
+                print("شماره همراه  یا کدملی اشتباه است")
         else:
-            user = CustomUser(username=username)
+            user = CustomUser(username=username, n_code=username)
             user.set_password(password)
-            print(username)
-            print(password)
             user.save()
             return redirect(reverse('client:home'))
-        
+
     return render(request, 'auth.html')
