46e1442e515d9ca580fa279b9df8cfa7438187ae
khaneara <hehehe3bar@gmail.com>
2024-12-10 13:43:18 +0330

    anewhere

--- sokht/settings.py
+++ sokht/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'saei1d$fuel',  # نام دیتابیس شما
         'USER': 'saei1d',                # نام کاربری
-        'PASSWORD': '1010S1010s',  # رمزعبور دیتابیس شما
+        'PASSWORD': '',  # رمزعبور دیتابیس شما
         'HOST': 'saei1d.mysql.pythonanywhere-services.com',  # آدرس هاست
         'PORT': '3306',
     }
